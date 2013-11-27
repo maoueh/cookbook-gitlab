5a69254c6ff8e483850f0918c6349f6de5c5a20e
Marin Jankovski <marin@gitlab.com>
2013-11-27 15:03:48 +0100

    Specify uid and gid for development.

--- recipes/users.rb
+++ recipes/users.rb
@@ -6,11 +6,19 @@
 gitlab = node['gitlab']
 
 # 3. System Users
+
+## Create group for GitLab user
+group gitlab['group'] do
+  gid gitlab['user_gid']
+end
+
 ## Create user for Gitlab.
 user gitlab['user'] do
   comment "GitLab user"
   home gitlab['home']
   shell "/bin/bash"
+  uid gitlab['user_uid']
+  gid gitlab['user_gid']
   supports :manage_home => true
 end
 
