8935af268ac615c102434bae52ce18cc93bec467
Jacob Vosmaer <contact@jacobvosmaer.nl>
2013-11-29 10:37:35 +0100

    Suppress grep output in git_login.sh

--- git_login.sh
+++ git_login.sh
@@ -4,4 +4,4 @@ if [ ! -f /home/git/.profile ]; then
   echo 'echo You are now logged in as the git user that runs GitLab, to get sudo privileges please exit to become the vagrant user' | su git -c 'cat >> /home/git/.profile'
 fi
 
-cat /home/vagrant/.bashrc | grep 'sudo su - git' || echo 'sudo su - git' >> /home/vagrant/.bashrc
+cat /home/vagrant/.bashrc | grep --quiet 'sudo su - git' || echo 'sudo su - git' >> /home/vagrant/.bashrc
