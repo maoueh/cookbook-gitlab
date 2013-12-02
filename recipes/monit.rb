189477a8e2f2118db4df1c6b2710a52ffcb9972c
Marin Jankovski <marin@gitlab.com>
2013-12-02 15:03:02 +0100

    Supply more attributes and add documentation.

--- recipes/monit.rb
+++ recipes/monit.rb
@@ -2,12 +2,15 @@
 # Cookbook Name:: gitlab
 # Recipe:: monit
 #
-monitrc = node['gitlab']['monitrc']
+gitlab = node['gitlab']
+monitrc = gitlab['monitrc']
 
 include_recipe "monit::default"
 
 monit_monitrc "sidekiq" do
   variables ({
+    gitlab_user: gitlab['user'],
+    gitlab_path: gitlab['path'],
     sidekiq_pid_path: monitrc['sidekiq_pid_path'],
     notify_email: monitrc['notify_email']
   })
@@ -15,6 +18,8 @@ end
 
 monit_monitrc "unicorn" do
   variables ({
+    gitlab_user: gitlab['user'],
+    gitlab_path: gitlab['path'],
     unicorn_pid_path: monitrc['unicorn_pid_path'],
     notify_email: monitrc['notify_email']
   })
