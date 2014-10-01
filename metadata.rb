e3a32728e749e35556041fa5fb86ef0fa9b1778c
Matthieu Vachon <matthieu.ouellette-vachon@nuecho.com>
2014-10-01 07:40:16 -0400

    Bumped version to 0.7.3

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'marin@gitlab.com'
 license          'MIT'
 description      'Installs/Configures GitLab'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.7.2'
+version          '0.7.3'
 
 recipe "gitlab::default", "Installation"
 
