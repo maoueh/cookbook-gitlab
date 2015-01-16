b76d590e4ca4635ce93853e1722586dffc3de960
Matthieu Vachon <matthieu.ouellette-vachon@nuecho.com>
2015-01-15 23:56:59 -0500

    Updated for GitLab 7.6

--- attributes/default.rb
+++ attributes/default.rb
@@ -74,7 +74,7 @@ if node['gitlab']['env'] == "development"
   default['gitlab']['shell_revision'] = "master"
 else
   default['gitlab']['environments'] = %w{production}
-  default['gitlab']['revision'] = "7-5-stable" # Must be branch, otherwise GitLab update will run on each chef run
+  default['gitlab']['revision'] = "7-6-stable" # Must be branch, otherwise GitLab update will run on each chef run
   default['gitlab']['url'] = "http://localhost:80/"
   default['gitlab']['port'] = "80"
   default['gitlab']['ssh_port'] = "22"
