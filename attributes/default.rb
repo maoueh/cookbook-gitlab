6117c44865aac8e709b47bb9f28ee3e6b3f7bff8
Matthieu Vachon <matthieu.o.vachon@gmail.com>
2015-01-20 19:21:05 -0500

    Re-ordered attributes for an eventual more natural split (app, db, web)

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,84 +1,48 @@
-# Packages
-if platform_family?("rhel")
-  packages = %w{
-    libicu-devel libxslt-devel libyaml-devel libxml2-devel gdbm-devel libffi-devel zlib-devel openssl-devel
-    libyaml-devel readline-devel curl-devel openssl-devel pcre-devel mysql-devel gcc-c++
-    ImageMagick-devel ImageMagick cmake
-  }
-else
-  packages = %w{
-    build-essential zlib1g-dev libyaml-dev libssl-dev libgdbm-dev libreadline-dev libncurses5-dev libffi-dev
-    curl openssh-server checkinstall libxml2-dev libxslt-dev libcurl4-openssl-dev libicu-dev python-docutils
-    logrotate vim curl wget checkinstall cmake
-  }
-end
+# GitLab Common Attributes
 
-default['gitlab']['packages'] = packages
-default['gitlab']['compile_ruby'] = true
-default['gitlab']['ruby'] = "2.1.2"
+## User
 
-# User
 default['gitlab']['user'] = "git"
 default['gitlab']['group'] = "git"
 default['gitlab']['user_uid'] = nil # Use to specify user id.
 default['gitlab']['user_gid'] = nil # Use to specify group id.
 default['gitlab']['home'] = "/home/git"
 
-# GitLab hq
+# GitLab App Server Attributes
+
+default['gitlab']['revision'] = "7-6-stable"
+default['gitlab']['url'] = "http://localhost:80/"
+default['gitlab']['port'] = "80"
+
+default['gitlab']['repository'] = "https://github.com/gitlabhq/gitlabhq.git"
+default['gitlab']['deploy_key'] = "" # Optional. Private key used to connect to private GitLab repository.
+
 default['gitlab']['path'] = "#{node['gitlab']['home']}/gitlab"
+default['gitlab']['repos_path'] = "#{node['gitlab']['home']}/repositories"
 default['gitlab']['satellites_path'] = "#{node['gitlab']['home']}/gitlab-satellites"
 default['gitlab']['satellites_timeout'] = 30
 
-# GitLab shell
 default['gitlab']['shell_repository'] = "https://github.com/gitlabhq/gitlab-shell.git"
 default['gitlab']['shell_path'] = "#{node['gitlab']['home']}/gitlab-shell"
 default['gitlab']['shell_revision'] = "v2.4.0"
 
-# GitLab shell configuration
-include_attribute 'redisio'
+## Backup
 
-default['gitlab']['repos_path'] = "#{node['gitlab']['home']}/repositories"
-default['gitlab']['redis_path'] = "/usr/local/bin/redis-cli"
-default['gitlab']['redis_socket_directory'] = "#{default['redisio']['base_piddir']}/sockets"
-default['gitlab']['redis_unixsocket'] = "#{default['gitlab']['redis_socket_directory']}/redis.sock" # To disable redis using Unix sockets set this value to nil
-if node['gitlab']['redis_unixsocket']
-  default['gitlab']['redis_port'] = "0"
-  default['gitlab']['redis_unixsocketperms'] = "0770"
-else
-  default['gitlab']['redis_port'] = "6379"
-  default['gitlab']['redis_unixsocketperms'] = nil
-end
-default['gitlab']['redis_host'] = "127.0.0.1"
-default['gitlab']['redis_database'] = nil # Default value is 0
-default['gitlab']['namespace']  = "resque:gitlab"
-default['gitlab']['self_signed_cert'] = false
+default['gitlab']['backup']['enable'] = true
+default['gitlab']['backup']['cron']['action'] = :create
+default['gitlab']['backup']['cron']['minute'] = 0
+default['gitlab']['backup']['cron']['hour'] = 2
+default['gitlab']['backup']['cron']['mailto'] = 'gitlab@localhost'
+default['gitlab']['backup']['cron']['path'] = '/usr/local/bin:/usr/bin:/bin'
+default['gitlab']['backup']['backup_keep_time'] = 0
+default['gitlab']['backup']['backup_path'] = 'tmp/backups'
 
-# Redis
-default['gitlab']['redis']['configure'] = true
-if node['gitlab']['redis']['configure']
-  default['redisio']['servers'] = [{'port' => node['gitlab']['redis_port'], 'address' => node['gitlab']['redis_host']}]
-  default['redisio']['default_settings']['unixsocket'] = node['gitlab']['redis_unixsocket']
-  default['redisio']['default_settings']['unixsocketperm'] = node['gitlab']['redis_unixsocketperms']
-end
+## Config
 
-# GitLab
-default['gitlab']['repository'] = "https://github.com/gitlabhq/gitlabhq.git"
-default['gitlab']['deploy_key'] = "" # Optional. Private key used to connect to private GitLab repository.
-
-# Setup environments
-
-default['gitlab']['revision'] = "7-6-stable" # Must be branch, otherwise GitLab update will run on each chef run
-default['gitlab']['url'] = "http://localhost:80/"
-default['gitlab']['port'] = "80"
-default['gitlab']['ssh_port'] = "22"
-
-# Nginx ip
-default['gitlab']['ip'] = "*"
-
-# GitLab configuration
 default['gitlab']['host'] = "localhost"
 default['gitlab']['email_enabled'] = true
 default['gitlab']['email_from'] = "gitlab@localhost"
+default['gitlab']['ssh_port'] = "22"
 
 default['gitlab']['timezone'] = "UTC"
 default['gitlab']['issue_closing_pattern'] = "([Cc]lose[sd]|[Ff]ixe[sd]) #(\d+)"
@@ -94,10 +58,10 @@ default['gitlab']['repository_downloads_path'] = "tmp/repositories"
 default['gitlab']['oauth_enabled'] = false
 default['gitlab']['oauth_block_auto_created_users'] = true
 default['gitlab']['oauth_allow_single_sign_on'] = false
-default['gitlab']['oauth_providers'] = [] # Example: default['gitlab']['oauth_providers'] = [ { "name": "google_oauth2", "app_id": "YOUR APP ID", "app_secret": "YOUR APP SECRET", "args": "access_type: 'offline', approval_prompt: ''" }, { "name": "twitter", "app_id": "YOUR APP ID", "app_secret": "YOUR APP SECRET" }, { "name":"github", "app_id": "YOUR APP ID", "app_secret": "YOUR APP SECRET" }]
+default['gitlab']['oauth_providers'] = []
 
-default['gitlab']['extra']['google_analytics_id'] = "" # Example:  "AA-1231231-1"
-default['gitlab']['extra']['sign_in_text'] = "" # Example:  "![Company Logo](http://www.example.com/logo.png)"
+default['gitlab']['extra']['google_analytics_id'] = ""
+default['gitlab']['extra']['sign_in_text'] = ""
 
 default['gitlab']['ldap']['enabled'] = false
 default['gitlab']['ldap']['label'] = "LDAP"
@@ -111,9 +75,6 @@ default['gitlab']['ldap']['password'] = "_the_password_of_the_bind_user"
 default['gitlab']['ldap']['allow_username_or_email_login'] = true
 default['gitlab']['ldap']['active_directory'] = true
 default['gitlab']['ldap']['allow_username_or_email_login'] = true
-
-# LDAP Filter Example: Recursive query of group membership
-# default['gitlab']['ldap']['user_filter'] = '(&(objectcategory=person)(objectclass=user)(memberOf:1.2.840.113556.1.4.1941:=CN=Gitlab Users,OU=USA,DC=int,DC=contoso,DC=com))'
 default['gitlab']['ldap']['user_filter'] = ''
 
 default['gitlab']['gravatar'] = true
@@ -127,30 +88,69 @@ default['gitlab']['default_projects_features']['snippets'] = false
 default['gitlab']['default_projects_features']['visibility_level'] = "private"
 
 default['gitlab']['webhook_timeout'] = 10
-
-# Default admin user password
 default['gitlab']['admin_root_password'] = nil
+default['gitlab']['unicorn_workers_number'] = 2
+default['gitlab']['unicorn_timeout'] = 60
 
-# Databases
+## Git
+
+default['git']['prefix'] = '/usr/local'
+default['git']['version'] = '2.1.4'
+default['git']['url'] = "https://github.com/git/git/archive/v#{node['git']['version']}.tar.gz"
+default['git']['checksum'] = '87ee4f10a64646daeaa4fed38da8afd48d60af18da51b59b6beea05345b06764'
+
+## Mail
+
+default['postfix']['mail_type'] = "client"
+default['postfix']['myhostname'] = "mail.localhost"
+default['postfix']['mydomain'] = "localhost"
+default['postfix']['myorigin'] = "mail.localhost"
+default['postfix']['smtp_use_tls'] = "no"
+
+default['gitlab']['smtp'] = {
+  :enabled => false,
+  :address => "email.server.com",
+  :port => 456,
+  :username => "smtp",
+  :password => "123456",
+  :domain => "gitlab.example.com",
+  :authentication => "login",
+  :enable_starttls_auto => true
+}
+
+## Packages
+
+case node["platform_family"]
+when "debian"
+  default['gitlab']['packages'] = %w{
+    build-essential zlib1g-dev libyaml-dev libssl-dev libgdbm-dev libreadline-dev libncurses5-dev libffi-dev
+    curl openssh-server checkinstall libxml2-dev libxslt-dev libcurl4-openssl-dev libicu-dev python-docutils
+    logrotate vim curl wget checkinstall cmake
+  }
+when "rhel"
+  default['gitlab']['packages'] = %w{
+    libicu-devel libxslt-devel libyaml-devel libxml2-devel gdbm-devel libffi-devel zlib-devel openssl-devel
+    libyaml-devel readline-devel curl-devel openssl-devel pcre-devel mysql-devel gcc-c++
+    ImageMagick-devel ImageMagick cmake
+  }
+end
+
+## Ruby
+
+default['gitlab']['compile_ruby'] = true
+default['gitlab']['ruby'] = "2.1.2"
+
+override['ruby_build']['install_git_pkgs'] = []
+
+# GitLab Database Server Attributes
+
 default['gitlab']['external_database'] = false
 default['gitlab']['database_adapter'] = "postgresql" # Or "mysql"
 default['gitlab']['database_password'] = "datapass"
 default['gitlab']['database_user'] = "git"
 default['gitlab']['database_allowed_host'] = "localhost" # Used by MySQL only
 
-# SELinux attributes
-default['selinux']['state'] = "disabled"
-
-# Ruby build attributes
-override['ruby_build']['install_git_pkgs'] = []
-
-# Git attributes
-default['git']['prefix'] = '/usr/local'
-default['git']['version'] = '2.1.4'
-default['git']['url'] = "https://github.com/git/git/archive/v#{node['git']['version']}.tar.gz"
-default['git']['checksum'] = '87ee4f10a64646daeaa4fed38da8afd48d60af18da51b59b6beea05345b06764'
-
-# MySQL attributes (server)
+## MySQL
 default['mysql']['server']['instance'] = "gitlab"
 default['mysql']['server']['host'] = "127.0.0.1"
 default['mysql']['server']['data_dir'] = nil
@@ -161,16 +161,21 @@ default['mysql']['server']['version'] = "5.5"
 default['mysql']['server']['username'] = "root"
 default['mysql']['server']['password'] = "rootpass"
 
-# PostgreSQL attributes
+default['selinux']['state'] = "disabled"
+
+## PostgreSQL
 include_attribute 'postgresql'
 
 default['postgresql']['version'] = "9.3"
+default['postgresql']['username']['postgres'] = "postgres"
+default['postgresql']['password']['postgres'] = "psqlpass"
+default['postgresql']['server']['host'] = "localhost"
+
 case node["platform_family"]
 when "debian"
   default['postgresql']['enable_pgdg_apt'] = true
   default['postgresql']['client']['packages'] = %w{postgresql-client-9.3 libpq-dev}
   default['postgresql']['server']['packages'] = %w{postgresql-9.3}
-  # due to the way attributes are organized we have to override the default paths too
   default['postgresql']['dir'] = "/etc/postgresql/#{node['postgresql']['version']}/main"
   default['postgresql']['config']['data_directory'] = "/var/lib/postgresql/#{node['postgresql']['version']}/main"
   default['postgresql']['config']['hba_file'] = "/etc/postgresql/#{node['postgresql']['version']}/main/pg_hba.conf"
@@ -189,47 +194,38 @@ when "rhel"
   default['postgresql']['server']['service_name'] = "postgresql-#{node['postgresql']['version']}"
   default['gitlab']['postgresql']['configuration_dir'] = "/usr/pgsql-#{node['postgresql']['version']}/bin"
 end
-default['postgresql']['username']['postgres'] = "postgres"
-default['postgresql']['password']['postgres'] = "psqlpass"
-default['postgresql']['server']['host'] = "localhost"
 
-# Postfix
-default['postfix']['mail_type'] = "client"
-default['postfix']['myhostname'] = "mail.localhost"
-default['postfix']['mydomain'] = "localhost"
-default['postfix']['myorigin'] = "mail.localhost"
-default['postfix']['smtp_use_tls'] = "no"
+## Redis
+include_attribute 'redisio'
 
-# Unicorn specific configuration
-default['gitlab']['unicorn_workers_number'] = 2
-default['gitlab']['unicorn_timeout'] = 60
+default['gitlab']['redis_path'] = "/usr/local/bin/redis-cli"
+default['gitlab']['redis_socket_directory'] = "#{default['redisio']['base_piddir']}/sockets"
+default['gitlab']['redis_unixsocket'] = "#{default['gitlab']['redis_socket_directory']}/redis.sock"
+if node['gitlab']['redis_unixsocket']
+  default['gitlab']['redis_port'] = "0"
+  default['gitlab']['redis_unixsocketperms'] = "0770"
+else
+  default['gitlab']['redis_port'] = "6379"
+  default['gitlab']['redis_unixsocketperms'] = nil
+end
+default['gitlab']['redis_host'] = "127.0.0.1"
+default['gitlab']['redis_database'] = nil # Default value is 0
+default['gitlab']['namespace']  = "resque:gitlab"
+default['gitlab']['self_signed_cert'] = false
 
-# Nginx & Nginx ssl certificates
+default['gitlab']['redis']['configure'] = true
+if node['gitlab']['redis']['configure']
+  default['redisio']['servers'] = [{'port' => node['gitlab']['redis_port'], 'address' => node['gitlab']['redis_host']}]
+  default['redisio']['default_settings']['unixsocket'] = node['gitlab']['redis_unixsocket']
+  default['redisio']['default_settings']['unixsocketperm'] = node['gitlab']['redis_unixsocketperms']
+end
+
+# GitLab Web Server Attributes
+
+default['gitlab']['ip'] = "*"
 default['gitlab']['install_nginx'] = true
 default['gitlab']['ssl_certificate_path'] = "/etc/ssl" # Path to .crt file. If it directory doesn't exist it will be created
 default['gitlab']['ssl_certificate_key_path'] = "/etc/ssl" # Path to .key file. If directory doesn't exist it will be created
 default['gitlab']['ssl_certificate'] = "" # SSL certificate
 default['gitlab']['ssl_certificate_key'] = "" # SSL certificate key
 default['gitlab']['client_max_body_size'] = "20m"
-
-# SMTP email
-default['gitlab']['smtp'] = {
-  :enabled => false,
-  :address => "email.server.com",
-  :port => 456,
-  :username => "smtp",
-  :password => "123456",
-  :domain => "gitlab.example.com",
-  :authentication => "login",
-  :enable_starttls_auto => true
-}
-
-# backups
-default['gitlab']['backup']['enable'] = true
-default['gitlab']['backup']['cron']['action'] = :create
-default['gitlab']['backup']['cron']['minute'] = 0
-default['gitlab']['backup']['cron']['hour'] = 2
-default['gitlab']['backup']['cron']['mailto'] = 'gitlab@localhost'
-default['gitlab']['backup']['cron']['path'] = '/usr/local/bin:/usr/bin:/bin'
-default['gitlab']['backup']['backup_keep_time'] = 0
-default['gitlab']['backup']['backup_path'] = 'tmp/backups'
