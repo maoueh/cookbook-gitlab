771b931c55e1420ca98d2e16959c0dd15ab7c587
Matthieu Vachon <matthieu.o.vachon@gmail.com>
2015-01-28 23:24:58 -0500

    Finialized serverspec tests

---
+++ test/integration/postgresql/serverspec/database_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe "PostgreSQL Database" do
+  it "is listening on port 5432" do
+    expect(port(5432)).to be_listening
+  end
+
+  it "has a running service named postgresql" do
+    if os[:family] == 'redhat'
+      expect(service("postgresql-9.3")).to be_running
+    else
+      expect(service("postgresql")).to be_running
+    end
+  end
+end
